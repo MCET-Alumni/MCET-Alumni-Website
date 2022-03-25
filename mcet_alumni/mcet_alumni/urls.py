5e048e80afd0d3223ff1991d4c485f6ae988aa04
Upendra0 <uk06704@gmail.com>
2022-03-25 23:21:04 +0530

    change of app names

--- mcet_alumni/mcet_alumni/urls.py
+++ mcet_alumni/mcet_alumni/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""mcet_alumni URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('alumni.urls')),
+    path('', include('home.urls')),
+    # path('/alumni', include('alumni.urls')),
     path('admin/', admin.site.urls),
 ]
+
--- mcet_alumni/mcet_alumni/wsgi.py
+++ mcet_alumni/mcet_alumni/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mysite project.
+WSGI config for mcet_alumni project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mcet_alumni.settings')
 
 application = get_wsgi_application()
