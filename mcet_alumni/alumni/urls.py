affab8163945b423e7f5716448ae95c81b1b83fb
atulya-karn <atulyak28@gmail.com>
2022-03-27 22:28:57 +0530

    added routing to different pages

--- mcet_alumni/alumni/urls.py
+++ mcet_alumni/alumni/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.alumni, name='alumni'),
-    path('notable_alumni', views.notable_alumni),
-    path('register', views.register),
+    path('notable_alumni', views.notable_alumni, name='notable_alumni'),
+    path('register', views.register, name='register'),
 ]
